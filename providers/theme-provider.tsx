63ff9a7b7eaea2ea6b026d57d29fcebc6c445b83
Hridya Agrawal <hridayahoney@gmail.com>
2025-06-06 18:35:00 +0000

    removed system and made white default since dark looks kinda worse for overall color scheme

--- providers/theme-provider.tsx
+++ providers/theme-provider.tsx
@@ -2,7 +2,7 @@
 
 import React, { createContext, useContext, useEffect, useState } from 'react'
 
-type Theme = 'dark' | 'light' | 'system'
+type Theme = 'dark' | 'light'
 
 type ThemeProviderProps = {
   children: React.ReactNode
@@ -16,7 +16,7 @@ type ThemeProviderState = {
 }
 
 const initialState: ThemeProviderState = {
-  theme: 'system',
+  theme: 'light',
   setTheme: () => null,
 }
 
@@ -24,7 +24,7 @@ const ThemeProviderContext = createContext<ThemeProviderState>(initialState)
 
 export function ThemeProvider({
   children,
-  defaultTheme = 'system',
+  defaultTheme = 'light',
   storageKey = 'bookify-ui-theme',
   ...props
 }: ThemeProviderProps) {
@@ -36,22 +36,10 @@ export function ThemeProvider({
       return defaultTheme
     }
   )
-
   useEffect(() => {
     const root = window.document.documentElement
 
     root.classList.remove('light', 'dark')
-
-    if (theme === 'system') {
-      const systemTheme = window.matchMedia('(prefers-color-scheme: dark)')
-        .matches
-        ? 'dark'
-        : 'light'
-
-      root.classList.add(systemTheme)
-      return
-    }
-
     root.classList.add(theme)
   }, [theme])
 
